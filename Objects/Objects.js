d160fc2b8189ce07a44f47ad6797465c39d2944b
Somnath Shinde <somnathshinde9855@gmail.com>
2025-02-18 19:21:12 +0530

    Object Obj2

---
+++ Objects/Objects.js
@@ -0,0 +1,15 @@
+// Syntax
+let obj = new Object();
+let obj1 = {};
+
+let  obj2 = {
+    name: "Peter Parker",
+    age: "28",
+    isMarried: () => "Yes",
+    wifeName: () => "Mary Jane Watson",
+    wife: {
+        name:"Mary Jane Watson",
+        age:"25",
+        DOB:"01/01/2000"
+    }
+}
